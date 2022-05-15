3f417fdb60332abd3ae87ec675c564bcfdf3c002
geebski <mlabenski123@gmail.com>
2022-05-15 03:28:53 -0400

    crazy commits at 3:30 degen

--- components/Filters/FilterChoices.vue
+++ components/Filters/FilterChoices.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <label>{{ filterName }}</label>
-    <select class="input">
-      <option v-for="choice in choices" :key="choice" :value="choice">
-        {{ choice }}
-      </option>
-    </select>
+    <span class="line" />
+    <h6 class="nav-item">
+      <a>{{ name }} </a><span class="btn-section"> - </span>
+    </h6>
+    <div v-for="choice in choices" :key="choice" :value="choice" class="checkbox">
+      <label>
+        <input type="checkbox"><span class="checkbox-material"><span class="check" /></span> {{ choice }}
+      </label>
+    </div>
   </div>
 </template>
 
@@ -13,9 +16,9 @@
 export default {
   name: 'FilterChoices',
   props: {
-    filterName: {
+    name: {
       type: String,
-      default: null
+      default: 'Hi'
     },
     choices: {
       type: Array,
@@ -28,4 +31,28 @@ export default {
 </script>
 
 <style scoped>
+.nav-item {
+  margin: 20px 0;
+}
+
+.nav-item a {
+  text-decoration: none;
+  color: black;
+  font-size: 1.5rem;
+  text-transform: capitalize;
+}
+
+.nav-item a:hover,
+.nav-item a:active {
+  color: red;
+}
+.checkbox {
+  text-transform: capitalize; }
+
+.line {
+  border-bottom: 1px solid #a5a5a5;
+  font-size: 1px;
+  display: block;
+  height: 1px;
+  margin-top: 30px; }
 </style>
