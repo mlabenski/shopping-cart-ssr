9e4ecb745eda7e664ae165d81dc2299624f0b7f1
geebski <mlabenski123@gmail.com>
2023-01-16 02:24:53 -0500

    test

--- components/Navigation/TheHeader.vue
+++ components/Navigation/TheHeader.vue
@@ -24,13 +24,13 @@
         <v-flex xs1>
 
         </v-flex>
-        <v-flex xs10>
+        <v-flex xs4>
           <nuxt-link to="/"><v-toolbar-title>{{ title }}</v-toolbar-title></nuxt-link>
         </v-flex>
     </v-layout>
       <v-layout row wrap>
-      <v-flex xs2>
-      </v-flex>
+        <v-spacer>
+        </v-spacer>
         <v-menu v-if="smallScreen">
         <template v-slot:activator="{ on }" class="div-for-small-screen">
           <v-btn icon v-on="on">
@@ -50,11 +50,12 @@
         </v-list>
       </v-menu>
 
-      <v-flex xs10 v-else>
-        <v-spacer></v-spacer>
+      <v-flex xs6 v-else>
         <v-btn text @click="onFilter">Categories</v-btn>
         <v-btn text @click="onCategories">Filters</v-btn>
         <v-badge color="primary" right><v-btn text @click="$router.push('/checkout')" :disabled="savedCart == 0">Checkout {{ savedCart }}</v-btn></v-badge>
+      </v-flex>
+      <v-flex xs2>
       </v-flex>
     </v-layout>
     </v-toolbar>
